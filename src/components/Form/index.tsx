f4008f64514644f5772b9dab4876ed32201a4fc4
Bruno Alcântara <brunoalcantarajc@gmail.com>
2021-09-02 11:19:49 -0300

    Update index.tsx

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import { addTodo, addTodoAsync } from '../../redux/features/todos'
 import { useAppDispatch } from '../../redux/hooks'
 
 const Form = () => {
   const dispatch = useAppDispatch()
   const [text, setText] = useState('')
+  
+   const handleAdd = useCallback(() => {
+    dispatch(addTodo(text))
+    setText('')
+  }, [text])
+
+  const handleAddAsync = useCallback(() => {
+    dispatch(addTodoAsync(text))
+    setText('')
+  }, [text])
 
   return (
     <div>
@@ -13,10 +23,10 @@ const Form = () => {
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <button type="submit" onClick={() => dispatch(addTodo(text))}>
+      <button type="submit" onClick={() => handleAdd()}>
         Add
       </button>
-      <button type="submit" onClick={() => dispatch(addTodoAsync(text))}>
+      <button type="submit" onClick={() => handleAddAsync()}>
         AddAsync
       </button>
     </div>
